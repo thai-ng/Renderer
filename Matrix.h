f471f5a5df806fb119f640b6313d9470f904a4e7
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-02-23 22:34:56 -0800

    start work on Matrix template

--- Matrix.h
+++ Matrix.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <array>
+
+template <int width, int height, typename T>
+class Matrix
+{
+using row_t = std::array<T, width>;
+using col_t = std::array<T, height>;
+
+public:
+	row_t getRow(int row)
+	{
+		
+	}
+private:
+	
+	std::array<T, width*height> _data;
+};
