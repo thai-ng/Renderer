e413bc439c86fd61cbe5fe8ca1a32bfdddc7caf1
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-02-25 17:07:38 -0800

    Working on rendering engine

--- RenderingEngine.hpp
+++ RenderingEngine.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <cmath>
 
 #include "primitives.h"
 #include "lerp.h"
+#include "Color.h"
 
 using Vector4_t = std::array<double, 4>;
 using Triangle_t = std::array<Vector4_t, 3>;
@@ -15,8 +17,40 @@ public:
 		Filled,
 		Wireframe
 	};
-	void RenderTriangle(const Triangle_t& triangle, RenderMode renderMode);
-	void RenderLine(const Line_t& triangle, RenderMode renderMode);
+	void RenderTriangle(const Triangle_t& triangle, RenderMode renderMode)
+	{
+		// Translate to screen space
+		Point point1 = Point{ static_cast<int>(std::round(triangle[0][0])), static_cast<int>(std::round(triangle[0][1])), static_cast<int>(std::round(triangle[0][2])), nullptr, getColorFromZ(static_cast<int>(std::round(triangle[0][2]))) };
+		Point point2 = Point{ static_cast<int>(std::round(triangle[1][0])), static_cast<int>(std::round(triangle[1][1])), static_cast<int>(std::round(triangle[1][2])), nullptr, getColorFromZ(static_cast<int>(std::round(triangle[0][2]))) };
+		Point point3 = Point{ static_cast<int>(std::round(triangle[2][0])), static_cast<int>(std::round(triangle[2][1])), static_cast<int>(std::round(triangle[2][2])), nullptr, getColorFromZ(static_cast<int>(std::round(triangle[0][2]))) };
+
+		renderPolygon(std::vector<Point>{ point1, point2, point3 }, nullptr, Color{ 0, 0, 0 }, &zBuffer);
+	}
+	void RenderLine(const Line_t& line, RenderMode renderMode)
+	{
+		// Translate to screen space
+
+		// Assign z Color
+		Point point1 = Point{ static_cast<int>(std::round(line[0][0])), static_cast<int>(std::round(line[0][1])), static_cast<int>(std::round(line[0][2])), nullptr, getColorFromZ(static_cast<int>(std::round(line[0][2]))) };
+		Point point2 = Point{ static_cast<int>(std::round(line[1][0])), static_cast<int>(std::round(line[1][1])), static_cast<int>(std::round(line[1][2])), nullptr, getColorFromZ(static_cast<int>(std::round(line[0][2]))) };
+
+		DDALineRenderer(point1, point2, nullptr, Color(0, 0, 0), 1.0, &zBuffer);
+	}
 
 private:
+	Color getColorFromZ(int z) const
+	{
+		auto r = static_cast<unsigned char>(redLerp[z].second);
+		auto g = static_cast<unsigned char>(greenLerp[z].second);
+		auto b = static_cast<unsigned char>(blueLerp[z].second);
+		return Color{ r, g, b };
+	}
+
+
+	Lerp<int> redLerp  = Lerp<int>(0, 200, 255, 0);
+	Lerp<int> greenLerp = Lerp<int>(0, 200, 255, 0);
+	Lerp<int> blueLerp = Lerp<int>(0, 200, 255, 0);
+	Matrix2D<int> zBuffer;
+	int zThreshold = 200;
+
 };
